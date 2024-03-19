ed57199f9240f9d0f82232340b7bf3232d7d0f69
Deeksha1511 <146502914+Deeksha1511@users.noreply.github.com>
2024-03-19 21:09:14 +0530

    Update Script.js

--- assets/js/index/Script.js
+++ assets/js/index/Script.js
@@ -2,7 +2,9 @@
 //Superhero Hunter App API Initialization
 (async function initialize() {
 	try {
-		const url = "https://superhero-hunter-app-mini-server.onrender.com";
+		const url = "https://gateway.marvel.com/v1/public/characters/a?apikey=5a4f5d89062e7fddfcd65cbe479e094c";
+
+		// const url = "https://superhero-hunter-app-mini-server.onrender.com";
 		const response = await fetch(url);
 		if (response.ok) console.log("API Running Successfully");
 		if (!response.ok) console.log("API Not Running");
